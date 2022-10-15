5e17d042f2b7dc36aaaa2f91aee5fd434e76fe05
Nguyễn Phi Long <98214053+CodeDebut@users.noreply.github.com>
2022-10-16 03:38:21 +0700

    QuizPractice

---
+++ src/main/java/com/example/quizv8/repositories/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.example.quizv8.repositories;
+
+import com.example.quizv8.model.QuestionDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<QuestionDetail, Long> {
+}
