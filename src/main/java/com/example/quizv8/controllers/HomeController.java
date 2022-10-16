7c669eb5946326824e0ac4edd59a3c5d2b620b4c
nbvan239 <86796935+nbvan239@users.noreply.github.com>
2022-10-16 23:12:13 +0700

    changes

--- src/main/java/com/example/quizv8/controllers/HomeController.java
+++ src/main/java/com/example/quizv8/controllers/HomeController.java
@@ -1,26 +1,11 @@
 package com.example.quizv8.controllers;
 
-import com.example.quizv8.model.QuizUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/" )
-    public String home() {
-        return "home";
-    }
-
-    @RequestMapping(value = "/homepage")
-    public String homePage() {
-        return "homePage";
-    }
-
-    @RequestMapping(value = "/profile")
-    public String profile() {
-        return "profile";
-    }
+    @RequestMapping("/")
+    public String home(){return "homePage";}
 }
