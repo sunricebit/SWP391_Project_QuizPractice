b285fb2434dc86cb3d00ea0fa128c39b1b88840a
Dinh Van Truong <truongdvhe153164@fpt.edu.vn>
2022-10-18 21:37:17 +0700

    Change entity

--- src/main/java/com/example/quizv8/service/IQuizListService.java
+++ src/main/java/com/example/quizv8/service/IQuizListService.java
@@ -1,5 +1,6 @@
 package com.example.quizv8.service;
 
+import com.example.quizv8.model.QuestionDetail;
 import com.example.quizv8.model.QuizList;
 import com.example.quizv8.model.QuizState;
 
@@ -7,12 +8,11 @@ import java.util.List;
 
 public interface IQuizListService {
     public List<QuizList> getQuizByUserID(long userId);
-    public boolean deleteAnswer(long questionId);
     public boolean deleteQuestionDetail(long quizListId);
     public boolean deleteQuizList(long quizListId);
     public QuizList getQuizListById(long QuizListId);
     public List<QuizList> getQuizPublic(long stateId);
 
     public List<QuizList> getQuizByCategory(String categoryName);
-
+    public List<QuestionDetail> getAllQuestion(long quizListID);
 }
